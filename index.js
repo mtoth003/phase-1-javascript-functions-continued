7b68acb9ee45d831cec3777c9c416cc8a1cdf7e1
Michael Toth <mtoth003@gmail.com>
2021-10-13 17:20:42 -0400

    commit tree 48b0c7a4fcc0c4ed02590d0761a4d7600bb20fcb

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = (task = "go to the office") => `This Monday, I will ${task}.` 
+
+function wrapAdjective(flair = "*") {
+    return function inner(adj = "Special") {
+         return `You are ${flair}${adj}${flair}!`;
+    }
+}
